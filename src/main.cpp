b2bd953991243eaedad7035ce5f251fb6db7db18
neuroDEVergent <braticpetar44@proton.me>
2026-04-22 16:20:01 +0200

    implemented MSAA and a custom post processing framebuffer

--- src/main.cpp
+++ src/main.cpp
@@ -39,11 +39,74 @@ int main( int argc, char* args[] )
 
   glEnable(GL_DEPTH_TEST);
 
+  float quadVertices[] = {   // vertex attributes for a quad that fills the entire screen in Normalized Device Coordinates.
+        // positions   // texCoords
+        -1.0f,  1.0f,  0.0f, 1.0f,
+        -1.0f, -1.0f,  0.0f, 0.0f,
+         1.0f, -1.0f,  1.0f, 0.0f,
+
+        -1.0f,  1.0f,  0.0f, 1.0f,
+         1.0f, -1.0f,  1.0f, 0.0f,
+         1.0f,  1.0f,  1.0f, 1.0f
+    };
+  // setup screen VAO
+  unsigned int quadVAO, quadVBO;
+  glGenVertexArrays(1, &quadVAO);
+  glGenBuffers(1, &quadVBO);
+  glBindVertexArray(quadVAO);
+  glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+  glEnableVertexAttribArray(1);
+  glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
+
+  // Configure MSAA framebuffer
+  unsigned int msaaframebuffer;
+  glGenFramebuffers(1, &msaaframebuffer);
+  glBindFramebuffer(GL_FRAMEBUFFER, msaaframebuffer);
+  // Create multisampled color attachment texture
+  unsigned int textureColorBufferMultiSampled;
+  glGenTextures(1, &textureColorBufferMultiSampled);
+  glBindTexture(GL_TEXTURE_2D_MULTISAMPLE, textureColorBufferMultiSampled);
+  glTexImage2DMultisample(GL_TEXTURE_2D_MULTISAMPLE, 4, GL_RGB, win.width, win.height,GL_TRUE);
+  glBindTexture(GL_TEXTURE_2D_MULTISAMPLE, 0);
+  glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D_MULTISAMPLE, textureColorBufferMultiSampled, 0);
+  // Create renderbuffer object for depth and stencil attachments
+  unsigned int rbo;
+  glGenRenderbuffers(1, &rbo);
+  glBindRenderbuffer(GL_RENDERBUFFER, rbo);
+  glRenderbufferStorageMultisample(GL_RENDERBUFFER, 4, GL_DEPTH24_STENCIL8, win.width, win.height);
+  glBindRenderbuffer(GL_RENDERBUFFER, 0);
+  glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo);
+
+  if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+    std::cout << "ERROR::FRAMEBUFFER:: Framebuffer is not complete!" << std::endl;
+  glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+  // Configure second post-processing framebuffer
+  unsigned int postprocessingframebuffer;
+  glGenFramebuffers(1, &postprocessingframebuffer);
+  glBindFramebuffer(GL_FRAMEBUFFER, postprocessingframebuffer);
+  // Create color attachment
+  unsigned int screenTexture;
+  glGenTextures(1, &screenTexture);
+  glBindTexture(GL_TEXTURE_2D, screenTexture);
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, win.width, win.height, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, screenTexture, 0);
+
+  if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+    std::cout << "ERROR::FRAMEBUFFER:: Screen Framebuffer is not complete!" << std::endl;
+  glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
   // build and compile shaders
   Shader planetShader("./shaders/planet-vs.glsl", "./shaders/planet-fs.glsl");
   Shader cubeMapShader("./shaders/cube-map-vs.glsl", "./shaders/cube-map-fs.glsl");
   Shader sunShader("./shaders/sun-vs.glsl","./shaders/sun-fs.glsl");
-  
+  Shader postProcessShader("./shaders/post-process-vs.glsl", "./shaders/post-process-fs.glsl");
+
   Planet planets[9];
   initializePlanets(planets, 9);
  
@@ -63,7 +126,7 @@ int main( int argc, char* args[] )
   };
   
   unsigned int spaceCubemap = loadCubemap(faces);
-  
+
   float simSpeed = 0.2;
 
   while (!win.quit)
@@ -78,13 +141,14 @@ int main( int argc, char* args[] )
     
     float simTime = time * simSpeed;
 
-    // Initialize clear color
-    // This is the background of the screen
     glViewport(0, 0, win.width, win.height);
     glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
-    // Clear color buffer and depth buffer
     glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
-
+    
+    // Draw the scene in multisampled buffers
+    glBindFramebuffer(GL_FRAMEBUFFER, msaaframebuffer);
+    glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glEnable(GL_DEPTH_TEST);
 
     // Declare matrices
@@ -139,6 +203,24 @@ int main( int argc, char* args[] )
     cubeMapShader.setMat4("view", glm::mat4(glm::mat3(camera.GetViewMatrix())));
     skyboxCube.Draw(cubeMapShader);
     glDepthFunc(GL_LESS);
+
+    // Now blit multisampled buffer to normal colorbuffer of intermediate FBO
+    glBindFramebuffer(GL_READ_FRAMEBUFFER, msaaframebuffer);
+    glBindFramebuffer(GL_DRAW_FRAMEBUFFER, postprocessingframebuffer);
+    glBlitFramebuffer(0, 0, win.width, win.height, 0, 0, win.width, win.height, GL_COLOR_BUFFER_BIT, GL_NEAREST);
+
+    // Now render quad with scene's visuals as it's texture image
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glDisable(GL_DEPTH_TEST);
+
+    // Draw screen quad
+    postProcessShader.use();
+    glBindVertexArray(quadVAO);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, screenTexture);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
 
     SDL_GL_SwapWindow(win.sdlWindow); 
     
